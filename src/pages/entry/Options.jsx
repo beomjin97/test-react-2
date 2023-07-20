c01f8b27ecb9c6778e2d0233b9b8b1ace8602dbd
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-07-20 18:07:33 +0900

    test and code alert for server response error

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -3,18 +3,23 @@ import axios from "axios";
 import ScoopOption from "./ScoopOption";
 import ToppingOption from "./ToppingOption";
 import Row from "react-bootstrap/Row";
+import AlertBanner from "../common/AlertBanner";
 
 const Options = ({ optionType }) => {
   const [items, setItems] = useState([]);
-
+  const [error, setError] = useState(false);
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
       .catch((error) => {
-        //TODO: handle error
+        setError(true);
       });
   }, [optionType]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
 
   const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
   const optionItems = items.map(({ name, imagePath }) => (
