553d1fcf903ee53b63d775b1e3d05c644b911ce1
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-07-21 18:52:34 +0900

    test for total sum

---
+++ src/pages/entry/test/totalUpdate.test.jsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Options from "../Options";
+
+test("updatescoop subtotal when scoops change", async () => {
+  render(<Options optionType="scoops" />);
+  const user = userEvent.setup();
+
+  const scoopsSubtotal = screen.getByText("Scoops total: $", { exact: false });
+  expect(scoopsSubtotal).toHaveTextContent("0.00");
+
+  const vanillaInput = await screen.findByRole("spinbutton", {
+    name: "Vanilla",
+  });
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "1");
+  expect(scoopsSubtotal).toHaveTextContent("2.00");
+
+  const chocolateInput = await screen.findByRole("spinbutton", {
+    name: "Chocolate",
+  });
+  await user.clear(chocolateInput);
+  await user.type(chocolateInput, "2");
+  expect(scoopsSubtotal).toHaveTextContent("6.00");
+});
