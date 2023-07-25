45ff4920aa9c48e2b7913baadc194bb47b41a7e7
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-07-26 01:39:55 +0900

    test and code subtotal for topping

--- src/pages/entry/test/totalUpdate.test.jsx
+++ src/pages/entry/test/totalUpdate.test.jsx
@@ -23,3 +23,24 @@ test("updatescoop subtotal when scoops change", async () => {
   await user.type(chocolateInput, "2");
   expect(scoopsSubtotal).toHaveTextContent("6.00");
 });
+
+test("update toppings subtotoal when toppings change", async () => {
+  const user = userEvent.setup();
+  render(<Options optionType={"toppings"} />);
+
+  const toppingsTotal = screen.getByText("Toppings total: $", { exact: false });
+  expect(toppingsTotal).toHaveTextContent("0.00");
+
+  const cherriesCheckbox = await screen.findByRole("checkbox", {
+    name: "Cherries",
+  });
+  await user.click(cherriesCheckbox);
+  expect(toppingsTotal).toHaveTextContent("1.50");
+
+  const hotFudgeCheckbox = screen.getByRole("checkbox", { name: "Hot fudge" });
+  await user.click(hotFudgeCheckbox);
+  expect(toppingsTotal).toHaveTextContent("3.00");
+
+  await user.click(hotFudgeCheckbox);
+  expect(toppingsTotal).toHaveTextContent("1.50");
+});
