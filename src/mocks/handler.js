8e8b65acfaf736cc1e1ef8444bc09630bf30f843
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-08-03 17:53:55 +0900

    order mock api

--- src/mocks/handler.js
+++ src/mocks/handler.js
@@ -1,5 +1,9 @@
 import { rest } from "msw";
 
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 export const handlers = [
   rest.get("http://localhost:3030/scoops", (req, res, ctx) => {
     return res(
@@ -16,6 +20,14 @@ export const handlers = [
         { name: "M&Ms", imagePath: "/images/m-and-ms.png" },
         { name: "Hot fudge", imagePath: "/images/hot-fudge.png" },
       ])
+    );
+  }),
+  rest.post("http://localhost:3030/order", async (req, res, ctx) => {
+    await sleep(100);
+    return res(
+      ctx.json({
+        orderNumber: 100,
+      })
     );
   }),
 ];
