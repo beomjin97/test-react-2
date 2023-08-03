14717d6f996c0a20370ee2641a9f2e5fc09e9260
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-08-03 15:48:42 +0900

    disable order button if no scoops ordered

--- src/pages/entry/OrderEntry.jsx
+++ src/pages/entry/OrderEntry.jsx
@@ -7,13 +7,17 @@ import Button from "react-bootstrap/Button";
 const OrderEntry = ({ setOrderPhase }) => {
   const { totals } = useOrderDetails();
 
+  const orderDisabled = totals.scoops === 0;
+
   return (
     <div>
       <h1>Design your sundae!</h1>
       <Options optionType="scoops" />
       <Options optionType="toppings" />
       <h2>Grand total: {formatCurrency(totals.scoops + totals.toppings)}</h2>
-      <Button onClick={() => setOrderPhase("review")}>Order Sundae!</Button>
+      <Button onClick={() => setOrderPhase("review")} disabled={orderDisabled}>
+        Order Sundae!
+      </Button>
     </div>
   );
 };
--- src/pages/entry/test/OrderEntry.test.jsx
+++ src/pages/entry/test/OrderEntry.test.jsx
@@ -6,6 +6,7 @@ import {
 import OrderEntry from "../OrderEntry";
 import { rest } from "msw";
 import { server } from "../../../mocks/server";
+import userEvent from "@testing-library/user-event";
 
 test("handle error for scoops and toppings routes", async () => {
   server.resetHandlers(
@@ -22,4 +23,23 @@ test("handle error for scoops and toppings routes", async () => {
     const alerts = await screen.findAllByRole("alert");
     expect(alerts).toHaveLength(2);
   });
+});
+
+test("disable order button if there are no scoops ordered", async () => {
+  const user = userEvent.setup();
+  render(<OrderEntry setOrderPhase={jest.fn()} />);
+
+  const orderButton = screen.getByRole("button", { name: /order sundae/i });
+  expect(orderButton).toBeDisabled();
+
+  const vanillaInput = await screen.findByRole("spinbutton", {
+    name: "Vanilla",
+  });
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "1");
+  expect(orderButton).toBeEnabled();
+
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "0");
+  expect(orderButton).toBeDisabled();
 });
